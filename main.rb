515b723b692158e687260905e29a6a9d7d8825b4
Koumarin <alicefigueiredo837@gmail.com>
2023-06-21 14:19:19 -0300

    initial commit

---
+++ main.rb
@@ -0,0 +1,38 @@
+#!/bin/env ruby
+require 'optparse'
+
+require 'curses'
+
+def with_curses
+  yield Curses.stdscr
+ensure
+  Curses.close_screen
+end
+
+OptionParser.new do |parser|
+  parser.banner = 'Usage: rmacs [file]'
+end.parse!
+
+if ARGV.size != 1
+  puts 'Please name a file to edit.'
+  exit
+end
+
+filename = ARGV.first
+
+with_curses do |stdscr|
+  File.open(filename, 'r+') do |file|
+    file.each do |line|
+      stdscr.addstr line
+    end
+
+    while true
+      c = stdscr.getch
+
+      case
+      when c == 'q'
+        break
+      end
+    end
+  end
+end
