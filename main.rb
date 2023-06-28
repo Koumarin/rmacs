c426fefbe9f2082a6adbfebb706922628dc39a1f
Koumarin <alicefigueiredo837@gmail.com>
2023-06-27 21:17:39 -0300

    refactor window drawing code out of text buffer

--- main.rb
+++ main.rb
@@ -7,7 +7,6 @@ class Buffer
   def initialize(path:)
     @file  = File.open(path, 'r+')
     @lines = []
-    @y, @x = 0, 0
 
     @file.each do |line|
       @lines.push line
@@ -19,36 +18,26 @@ class Buffer
     end
   end
 
-  def insert(c)
-    x = @x.clamp 0, @lines[@y].size - 1
+  def insert(c, x:, y:)
+    x = x.clamp 0, (line_size y) - 1
 
-    @lines[@y].insert x, c
-    @x += 1
+    @lines[y].insert x, c
   end
 
-  def split_line
-    new_line = @lines[@y][@x..@lines[@y].size]
+  def split_line(x:, y:)
+    new_line = @lines[y][x..(line_size y)]
 
-    @lines[@y] = @lines[@y][0, @x] + "\n"
-    @lines.insert @y + 1, new_line
-
-    move x: -@x, y: 1
+    @lines[y] = @lines[y][0, x] + "\n"
+    @lines.insert y + 1, new_line
   end
 
-  def backspace
-    unless @y == 0 and @x == 0
-      move x: -1
-      delete
-    end
-  end
-
-  def delete
-    @lines[@y].slice! @x
+  def delete(x:, y:)
+    @lines[y].slice! x
 
     ## If we deleted the end of line, we remove the new line.
-    if @x >= @lines[@y].size - 1
-      @lines[@y] += @lines[@y + 1]
-      @lines.delete_at @y + 1
+    if x >= (line_size y) - 1
+      @lines[y] += @lines[y + 1]
+      @lines.delete_at y + 1
     end
   end
 
@@ -62,40 +51,101 @@ class Buffer
     @file.flush
   end
 
+  def each_with_index
+    @lines.each_with_index do |line, idx|
+      yield line, idx
+    end
+  end
+
+  def bottom
+    @lines.size - 1
+  end
+
+  def line_size(y)
+    @lines[y].size
+  end
+end
+
+class Window
+  def initialize(screen:, buffer:)
+    ##                                 height          width     y  x
+    @curses_window = screen.subwin screen.maxy - 1, screen.maxx, 0, 0
+    @buffer        = buffer
+    @y, @x         = 0, 0
+  end
+
   def move(x: 0, y: 0)
-    @y  = (@y + y).clamp 0, @lines.size - 1
+    @y  = (@y + y).clamp 0, @buffer.bottom
     @x += x
 
     if x != 0
       case
-      when @x >= @lines[@y].size
-        @y = (@y + 1).clamp 0, @lines.size - 1
+      when @x >= (@buffer.line_size @y)
+        @y = (@y + 1).clamp 0, @buffer.bottom
         @x = 0
       when @x < 0
-        @y = (@y - 1).clamp 0, @lines.size - 1
-        @x = @lines[@y].size - 1
+        @y = (@y - 1).clamp 0, @buffer.bottom
+        @x = (@buffer.line_size @y) - 1
       end
     end
   end
 
-  def draw(to:)
+  def insert(c)
+    @buffer.insert c, x: @x, y: @y
+    move x: 1
+  end
+
+  def split_line
+    @buffer.split_line x: @x, y: @y
+
+    move x: -@x, y: 1
+  end
+
+  def backspace
+    unless @y == 0 and @x == 0
+      move x: -1
+      @buffer.delete x: @x, y: @y
+    end
+  end
+
+  def delete
+    @buffer.delete x: @x, y: @y
+  end
+
+  def draw
+    return if not @buffer
+
     ## We want to scroll the buffer until the current cursor is in the screen.
     off  = 0
-    off += to.maxy while @y - off >= to.maxy
-    stop = to.maxy + off
+    off += height while @y - off >= height
+    stop = height + off
 
-    to.setpos 0, 0
-    to.erase
+    @curses_window.setpos 0, 0
+    @curses_window.erase
 
-    @lines.each_with_index do |line, idx|
+    @buffer.each_with_index do |line, idx|
       next  if idx <  off
       break if idx >= stop
 
-      to.addstr line
+      @curses_window.addstr line
     end
 
-    to.setpos (@y - off).clamp(0, to.maxy),
-              @x.clamp(0, @lines[@y].size - 1)
+    @curses_window.setpos (@y - off).clamp(0, height),
+                          @x.clamp(0, (@buffer.line_size @y) - 1)
+
+    @curses_window.refresh
+  end
+
+  def save
+    @buffer.save
+  end
+
+  def height
+    @curses_window.maxy
+  end
+
+  def width
+    @curses_window.maxx
   end
 end
 
@@ -117,7 +167,8 @@ end
 filename = ARGV.first
 
 with_curses do |stdscr|
-  buffer = Buffer.new path: filename
+  window = Window.new screen: stdscr,
+                      buffer: (Buffer.new path: filename)
 
   Curses.curs_set 2                     # Make cursor visible.
   Curses.cbreak                         # Disable input buffering.
@@ -126,32 +177,32 @@ with_curses do |stdscr|
   stdscr.keypad true                    # Enable terminal keypad.
 
   loop do
-    buffer.draw to: stdscr
+    window.draw
 
     c = stdscr.getch
 
     case c
     when Curses::Key::LEFT
-      buffer.move x: -1
+      window.move x: -1
     when Curses::Key::RIGHT
-      buffer.move x: 1
+      window.move x: 1
     when Curses::Key::UP
-      buffer.move y: -1
+      window.move y: -1
     when Curses::Key::DOWN
-      buffer.move y: 1
+      window.move y: 1
     when 10
-      buffer.split_line
+      window.split_line
     when Curses::Key::BACKSPACE
-      buffer.backspace
+      window.backspace
     when Curses::Key::DC                # Delete character.
-      buffer.delete
+      window.delete
     ## Control characters:
     when 3                              # C-c
       break
     when 19                             # C-s
-      buffer.save
+      window.save
     else
-      buffer.insert c
+      window.insert c
     end
   end
 end
